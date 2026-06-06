e007eb4d30e92f22ff498562a8aa503fbec3755e
jaydeepthv <147241350+jaydeepthv@users.noreply.github.com>
2026-06-06 17:02:01 +0530

    Create PrettyPrinting.java

---
+++ String/PrettyPrinting.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String args[]){
+        
+        float pi =(float)Math.PI;
+        
+        // printf is used for formatted output
+        System.out.printf("PIE : %.2f %n",pi);
+        
+        System.out.printf("Myself %s and I am %s", "Jaydeep" , " Enginner");
+        
+        // %s      -> String
+        // %d      -> Integer
+        // %f      -> Decimal number
+        // %.2f    -> 2 decimal places
+        // %c      -> Character
+        // %b      -> Boolean
+        // %x      -> Hex
+        // %n      -> New line
+        // %%      -> %
+       
+    }
+}
