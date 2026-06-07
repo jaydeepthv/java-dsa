f3bf79450058718ae704f1884af16d70c670e8e6
jaydeepthv <147241350+jaydeepthv@users.noreply.github.com>
2026-06-07 17:04:50 +0530

    Create subSeqWithDup.java

---
+++ Array/subSeqWithDup.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main{
+    static List<List<Integer>> subSeqWithDup(int arr[]){
+        Arrays.sort(arr);
+        
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        
+        int start = 0;
+        int end = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            start = 0;
+            if(i>0 && arr[i] == arr[i-1]){
+                start = end + 1;
+            }
+            
+            end = outer.size() - 1;
+            int n = outer.size();
+            
+            for(int j = start ; j<n ; j++){
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+        
+    }
+    
+    public static void main(String Args[]){
+        int arr[] = {1,2,2};
+       List<List<Integer>> ssArr= subSeqWithDup(arr);
+       System.out.println(ssArr);
+        
+    }
+}
